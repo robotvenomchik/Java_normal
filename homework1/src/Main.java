b4b6d9f263ae1dbc47948a7b114705b20e2dcae6
robotvenomchik <96344010+robotvenomchik@users.noreply.github.com>
2024-01-12 20:50:51 +0200

    homework commit

---
+++ homework1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String pepa="dfasfa";
+        int integer=213;
+
+        System.out.println("Hello world!");
+        System.out.println(pepa);
+        System.out.println(integer);
+    }
+
+}
